68af89c33edb6ee898a9fee08001630cb0db7d06
Liam Kinne <liam@kinne.io>
2023-06-25 22:30:30 +1000

    fix edit text

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,9 +2,6 @@ export default {
   logo: <img src="/logo.svg" style={{ height: "3em" }} />,
   docsRepositoryBase: 'https://github.com/optimize-business/docs',
   darkMode: false,
-  editLink: {
-    text: "What"
-  },
   nextThemes: {
     forcedTheme: 'light'
   }
