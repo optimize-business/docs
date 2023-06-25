29fee62aa6e74742ee1dd1ccb3554f487e85df11
Liam Kinne <liam@kinne.io>
2023-06-25 21:22:07 +1000

    add inital stuff

---
+++ theme.config.jsx
@@ -0,0 +1,11 @@
+export default {
+  logo: <img src="/logo.svg" style={{ height: "3em" }} />,
+  docsRepositoryBase: 'https://github.com/optimize-business/docs',
+  darkMode: false,
+  editLink: {
+    text: "What"
+  },
+  nextThemes: {
+    forcedTheme: 'light'
+  }
+}
