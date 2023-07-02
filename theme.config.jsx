9680ca3f68d7fdd54c783dcfaaa1e292f1d75e14
Liam Kinne <liam@kinne.io>
2023-07-02 23:28:45 +1000

    add favicon

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,10 @@
 export default {
   logo: <img src="/logo.svg" style={{ height: "3em" }} />,
+  head: (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  ),
   docsRepositoryBase: 'https://github.com/optimize-business/docs',
   darkMode: false,
   nextThemes: {
